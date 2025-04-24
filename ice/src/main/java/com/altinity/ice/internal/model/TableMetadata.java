0560c40b209ba22ddc5cb9c544ee22f2881f2a99
Kanthi Subramanian <subkanthi@gmail.com>
2025-04-24 17:44:20 -0400

    ice: Removed lambok depedency and replaced with record.

--- ice/src/main/java/com/altinity/ice/internal/model/TableMetadata.java
+++ ice/src/main/java/com/altinity/ice/internal/model/TableMetadata.java
@@ -3,65 +3,39 @@ package com.altinity.ice.internal.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 import java.util.Map;
-import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Data
-public class TableMetadata {
-  private String kind = "Table";
-  private Metadata metadata;
-  private TableData data;
-
-  public void setMetadata(Metadata metadata) {
-    this.metadata = metadata;
+public record TableMetadata(String kind, Metadata metadata, TableData data) {
+  public TableMetadata {
+    if (kind == null) {
+      kind = "Table";
+    }
   }
 
-  public void setData(TableData data) {
-    this.data = data;
-  }
+  public record Metadata(String id) {}
 
-  @Data
-  public static class Metadata {
-    private String id;
-  }
+  public record TableData(
+      String schema_raw,
+      String partition_spec_raw,
+      String sort_order_raw,
+      Map<String, String> properties,
+      String location,
+      SnapshotInfo current_snapshot,
+      List<MetricsInfo> metrics) {}
 
-  @Data
-  public static class TableData {
-    private String schema_raw;
-    private String partition_spec_raw;
-    private String sort_order_raw;
-    private Map<String, String> properties;
-    private String location;
-    private SnapshotInfo current_snapshot;
-    private List<MetricsInfo> metrics;
-  }
+  public record SnapshotInfo(
+      long sequence_number,
+      long id,
+      Long parent_id,
+      long timestamp,
+      String timestamp_iso,
+      String timestamp_iso_local,
+      String operation,
+      Map<String, String> summary,
+      String location) {}
 
-  @Data
-  public static class SnapshotInfo {
-    private long sequence_number;
-    private long id;
-    private Long parent_id;
-    private long timestamp;
-    private String timestamp_iso;
-    private String timestamp_iso_local;
-    private String operation;
-    private Map<String, String> summary;
-    private String location;
-  }
+  public record MetricsInfo(String file, long record_count, List<ColumnMetrics> columns) {}
 
-  @Data
-  public static class MetricsInfo {
-    private String file;
-    private long record_count;
-    private List<ColumnMetrics> columns;
-  }
-
-  @Data
-  public static class ColumnMetrics {
-    private String name;
-    private Long value_count;
-    private Long null_count;
-    private String lower_bound;
-    private String upper_bound;
-  }
+  public record ColumnMetrics(
+      String name, Long value_count, Long null_count, String lower_bound, String upper_bound) {}
 }
