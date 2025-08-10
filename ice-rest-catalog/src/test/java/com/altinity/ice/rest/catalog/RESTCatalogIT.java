3d793e1b89f2e9633f5afe8cbe9bc3c197c9d470
Kanthi Subramanian <subkanthi@gmail.com>
2025-08-10 17:50:27 -0500

    Fixed formatting errors.

--- ice-rest-catalog/src/test/java/com/altinity/ice/rest/catalog/RESTCatalogIT.java
+++ ice-rest-catalog/src/test/java/com/altinity/ice/rest/catalog/RESTCatalogIT.java
@@ -9,8 +9,8 @@
  */
 package com.altinity.ice.rest.catalog;
 
-import java.io.File;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.PrintStream;
 import org.testng.annotations.Test;
 import picocli.CommandLine;
@@ -95,46 +95,56 @@ public class RESTCatalogIT extends RESTCatalogTestBase {
     // Save original System.out and System.err
     PrintStream originalOut = System.out;
     PrintStream originalErr = System.err;
-    
+
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
     PrintStream captureOut = new PrintStream(outputStream);
     PrintStream captureErr = new PrintStream(errorStream);
-    
+
     try {
       // Redirect System.out and System.err to capture streams
       System.setOut(captureOut);
       System.setErr(captureErr);
-      
-      int scanExitCode = new CommandLine(com.altinity.ice.cli.Main.class)
-          .execute("--config", tempConfigFile.getAbsolutePath(), "scan", tableName);
-      
+
+      int scanExitCode =
+          new CommandLine(com.altinity.ice.cli.Main.class)
+              .execute("--config", tempConfigFile.getAbsolutePath(), "scan", tableName);
+
       captureOut.flush();
       captureErr.flush();
-      
+
       String scanOutput = outputStream.toString();
       String scanError = errorStream.toString();
-      
+
       // Combine stdout and stderr for analysis
       String combinedOutput = scanOutput + scanError;
-    
+
       // Verify scan command succeeded
       assert scanExitCode == 0 : "Scan command should succeed on existing table";
-      
+
       // Validate scan output contains expected data from iris dataset
       assert combinedOutput.length() > 0 : "Scan should produce some output";
-      
+
       // Check for iris dataset columns (be flexible with column name formats)
-      boolean hasSepalLength = combinedOutput.contains("sepal.length") || combinedOutput.contains("sepal_length") || combinedOutput.contains("sepal-length");
+      boolean hasSepalLength =
+          combinedOutput.contains("sepal.length")
+              || combinedOutput.contains("sepal_length")
+              || combinedOutput.contains("sepal-length");
       boolean hasVariety = combinedOutput.contains("variety");
-      
-      assert hasSepalLength : "Scan output should contain sepal length column data. Output: " + combinedOutput.substring(0, Math.min(500, combinedOutput.length()));
-      assert hasVariety : "Scan output should contain variety column data. Output: " + combinedOutput.substring(0, Math.min(500, combinedOutput.length()));
-      
+
+      assert hasSepalLength
+          : "Scan output should contain sepal length column data. Output: "
+              + combinedOutput.substring(0, Math.min(500, combinedOutput.length()));
+      assert hasVariety
+          : "Scan output should contain variety column data. Output: "
+              + combinedOutput.substring(0, Math.min(500, combinedOutput.length()));
+
       logger.info("ICE CLI scan command test successful - validated output contains iris data");
       logger.info("Scan output length: {} characters", combinedOutput.length());
-      logger.debug("Scan output preview: {}", combinedOutput.substring(0, Math.min(500, combinedOutput.length())));
-      
+      logger.debug(
+          "Scan output preview: {}",
+          combinedOutput.substring(0, Math.min(500, combinedOutput.length())));
+
     } finally {
       // Restore original System.out and System.err
       System.setOut(originalOut);
