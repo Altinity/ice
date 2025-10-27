157b49991a5eddc260db5058a92a59c812a0d300
ruslansenchukov <ruslan.senchukov@daniam.am>
2025-10-27 11:36:14 +0400

    fixed formations

--- ice/src/main/java/com/altinity/ice/cli/internal/http/MinioWildcard.java
+++ ice/src/main/java/com/altinity/ice/cli/internal/http/MinioWildcard.java
@@ -8,7 +8,6 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
-
 package com.altinity.ice.cli.internal.http;
 
 import java.net.URI;
@@ -17,39 +16,36 @@ import java.util.*;
 import org.json.*;
 
 public final class MinioWildcard {
-    private static final HttpClient client = HttpClient.newHttpClient();
+  private static final HttpClient client = HttpClient.newHttpClient();
 
-    public static List<String> listHTTPWildcard(String urlWithStar) throws Exception {
-        URI uri = URI.create(urlWithStar);
-        String[] parts = uri.getPath().split("/", 3);
-        if (parts.length < 2)
-            throw new IllegalArgumentException("Bad MinIO URL: " + urlWithStar);
+  public static List<String> listHTTPWildcard(String urlWithStar) throws Exception {
+    URI uri = URI.create(urlWithStar);
+    String[] parts = uri.getPath().split("/", 3);
+    if (parts.length < 2) throw new IllegalArgumentException("Bad MinIO URL: " + urlWithStar);
 
-        String bucket = parts[1];
-        String prefix = parts.length == 3 ? parts[2].replaceAll("\\*", "") : "";
-        String endpoint = uri.getScheme() + "://" + uri.getHost() + ":" + uri.getPort();
+    String bucket = parts[1];
+    String prefix = parts.length == 3 ? parts[2].replaceAll("\\*", "") : "";
+    String endpoint = uri.getScheme() + "://" + uri.getHost() + ":" + uri.getPort();
 
-        String listUrl = endpoint + "/" + bucket + "?list-type=2&prefix=" + prefix;
-        HttpRequest req = HttpRequest.newBuilder(URI.create(listUrl)).GET().build();
-        HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandlers.ofString());
-        if (resp.statusCode() != 200)
-            throw new RuntimeException("MinIO listObjects error: " + resp.statusCode());
+    String listUrl = endpoint + "/" + bucket + "?list-type=2&prefix=" + prefix;
+    HttpRequest req = HttpRequest.newBuilder(URI.create(listUrl)).GET().build();
+    HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandlers.ofString());
+    if (resp.statusCode() != 200)
+      throw new RuntimeException("MinIO listObjects error: " + resp.statusCode());
 
-        JSONObject xml = org.json.XML.toJSONObject(resp.body());
-        List<String> files = new ArrayList<>();
-        var result = xml.getJSONObject("ListBucketResult");
-        var contents = result.optJSONArray("Contents");
-        if (contents != null) {
-            for (int i = 0; i < contents.length(); i++) {
-                String key = contents.getJSONObject(i).getString("Key");
-                if (key.endsWith(".parquet"))
-                    files.add(endpoint + "/" + bucket + "/" + key);
-            }
-        } else {
-            var single = result.optJSONObject("Contents");
-            if (single != null)
-                files.add(endpoint + "/" + bucket + "/" + single.getString("Key"));
-        }
-        return files;
+    JSONObject xml = org.json.XML.toJSONObject(resp.body());
+    List<String> files = new ArrayList<>();
+    var result = xml.getJSONObject("ListBucketResult");
+    var contents = result.optJSONArray("Contents");
+    if (contents != null) {
+      for (int i = 0; i < contents.length(); i++) {
+        String key = contents.getJSONObject(i).getString("Key");
+        if (key.endsWith(".parquet")) files.add(endpoint + "/" + bucket + "/" + key);
+      }
+    } else {
+      var single = result.optJSONObject("Contents");
+      if (single != null) files.add(endpoint + "/" + bucket + "/" + single.getString("Key"));
     }
+    return files;
+  }
 }
