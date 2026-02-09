b4ff749bc54dfe71cb26a1582468c9181bf33f6d
Andrew Xie <dev@xie.is>
2026-02-09 14:43:10 -0500

    fix: Prevent empty namespace creation

--- ice/src/main/java/com/altinity/ice/cli/Main.java
+++ ice/src/main/java/com/altinity/ice/cli/Main.java
@@ -628,7 +628,15 @@ public final class Main {
           boolean createNamespaceIfNotExists)
       throws IOException {
     try (RESTCatalog catalog = loadCatalog()) {
-      CreateNamespace.run(catalog, Namespace.of(name.split("[.]")), createNamespaceIfNotExists);
+      String[] split = name.split("[.]");
+      for (String level : split) {
+        if (level.isEmpty()) {
+          throw new IllegalArgumentException(
+              "Invalid namespace name: '.' cannot separate empty names");
+        }
+      }
+
+      CreateNamespace.run(catalog, Namespace.of(split), createNamespaceIfNotExists);
     }
   }
 
